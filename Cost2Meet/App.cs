9d7354e241e516180f2160835dd9c0d81e3a4532
Venkata Koppaka <venkata@falafel.com>
2014-10-14 22:45:53 -0700

    adding sqlite repository and table classes

--- Cost2Meet/App.cs
+++ Cost2Meet/App.cs
@@ -8,9 +8,13 @@ namespace Cost2Meet
 		public static readonly string InsightsKey = "<ADD YOUR KEY HERE>";
 		public static readonly string DatabaseName = "cost2meet.db3";
 
+		public static Cost2MeetRepository Cost2MeetRepository { get; private set; }
+
 
 		public static Page GetMainPage (ISQLitePlatform sqlitePlatform, string dbFilePath)
 		{	
+			Cost2MeetRepository = new Cost2MeetRepository ( sqlitePlatform, dbFilePath );
+
 			var mainPage = new MainPage ( );
 			return new NavigationPage ( mainPage );
 		}
---
+++ Cost2Meet/Data/Cost2MeetRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SQLite.Net.Async;
+using SQLite.Net.Interop;
+using SQLite.Net;
+
+namespace Cost2Meet
+{
+	public class Cost2MeetRepository
+	{
+		private SQLiteAsyncConnection dbConn;
+
+		public Cost2MeetRepository ( ISQLitePlatform platform, string dbPath )
+		{
+			var connectionFunc = new Func<SQLiteConnectionWithLock> ( ( ) =>
+				new SQLiteConnectionWithLock (
+					platform,
+					new SQLiteConnectionString ( dbPath, false )
+				)
+			);
+
+			dbConn = new SQLiteAsyncConnection ( connectionFunc );
+			dbConn.CreateTableAsync<MeetingHistory>( );
+		}
+	}
+}
