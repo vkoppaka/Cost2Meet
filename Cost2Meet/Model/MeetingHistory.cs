b7f3020d9e175781772ff38b1156ec7cf01e1c33
Venkata Koppaka <venkata@falafel.com>
2014-10-14 22:46:05 -0700

    Meeting history class

---
+++ Cost2Meet/Model/MeetingHistory.cs
@@ -0,0 +1,14 @@
+﻿using SQLite.Net.Attributes;
+
+namespace Cost2Meet
+{
+	[Table("meetinghistory")]
+	public class MeetingHistory
+	{
+		[Unique, PrimaryKey]
+		public int ID { get; set; }
+
+		public string History { get; set; }
+	}
+}
+
